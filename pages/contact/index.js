a1578b0db72c79fbf87093a01b0233ccc10f74b3
Lisandro Bigi <lisandrobigi@MacBook-Pro-de-Lisandro.local>
2021-02-05 16:34:37 -0300

    subscription form with netlify connection

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head'
 import Navbar from '../../components/Shared/Navbar'
+import Footer from '../../components/Shared/Footer'
 
 export default function Home() {
   return (
@@ -37,24 +38,24 @@ export default function Home() {
                 <input type="hidden" name="form-name" value="contact" />
                   <div className="form-group">
                     <label>Name</label>
-                    <input type="email" name="name" className="form-control bg-transparent border border-secondary" placeholder="Name" />
+                    <input type="text" name="name" className="form-control bg-transparent border border-secondary" placeholder="Name" required />
                   </div>
                   <div className="form-group">
                     <label>Email</label>
-                    <input type="email" name="email" className="form-control bg-transparent border border-secondary" placeholder="Email" />
+                    <input type="email" name="email" className="form-control bg-transparent border border-secondary" placeholder="Email" required />
                   </div>
                   <div className="form-group">
                     <label>Subject</label>
-                    <input type="text" name="subject" className="form-control bg-transparent border border-secondary" placeholder="Subject" />
+                    <input type="text" name="subject" className="form-control bg-transparent border border-secondary" placeholder="Subject" required />
                   </div>
                   <div className="form-group">
                     <label>Message</label>
-                    <textarea name="message" className="form-control bg-transparent border border-secondary" placeholder="Message" />
+                    <textarea name="message" className="form-control bg-transparent border border-secondary" placeholder="Message" required />
                   </div>
                   <div className="row align-items-center">
                     <div className="col-7">
                     <div className="form-check">
-                      <input className="form-check-input" type="checkbox" name="terms" />
+                      <input className="form-check-input" type="checkbox" name="termsConditions" required />
                       <label className="form-check-label">
                         Accept terms & conditions
                       </label>
@@ -69,41 +70,7 @@ export default function Home() {
           </div>
         </div>
     
-      <footer className="bg-primary py-5 text-white mt-3">
-        <div className="container">
-          <div className="d-flex align-items-center mt-3 mb-5" style={{justifyContent: 'space-evenly'}}>
-            <div>
-            <h3 className="text-white">Leave us your mail to subscribe<br /> to our weekly blogposts</h3>
-            </div>
-            <div>
-              <form className="form-inline">
-                <div className="form-group mx-sm-3 mb-2">
-                  <input type="email" className="form-control" id="inputPassword2" placeholder="Email" />
-                </div>
-                <button type="submit" className="btn btn-light text-primary mb-2">Subscribe</button>
-              </form>
-            </div>
-          </div>
-          <h3 className="text-white text-center">We are available for new projects on all major hiring platforms</h3>
-          <div className="social-icons py-5 text-center">
-            <a className="pr-5"  href="https://www.upwork.com/agencies/~01b0561df3bf4ea42c" rel="noopener noreferrer" target="_blank">
-              <img width="10%" src="./Upwork.png" alt="upwork logo"></img>
-            </a>
-            <a className="pr-5" href="https://www.linkedin.com/company/pengyilabs/" rel="noopener noreferrer" target="_blank">
-              <img width="10%" src="./linkedin.svg" alt="linkedin logo"></img>
-            </a>
-            <a className="pr-5" href="https://dribbble.com/pengyilabs" rel="noopener noreferrer" target="_blank">
-              <img width="10%" src="./dribbble.svg" alt="dribbble logo"></img>
-            </a>
-            <a className="pr-5" href="https://clutch.co/profile/pengyi-labs" rel="noopener noreferrer" target="_blank">
-              <img width="10%" src="./clutch.png" alt="clutch logo"></img>
-            </a>
-          </div>
-          <p className="my-2 text-center">
-            At PengYi we are excited to take on new challenges, if you rather contact us directly please write an email to <u><a href="mailto:hello@pengyilabs.io" className="text-white">hello@pengyilabs.io</a></u>
-          </p>
-        </div>
-      </footer>
+        <Footer />
     </div>
     </div>
   )
