a001ad265df28b0fe8d87aba7fdaa9498f1217ae
Roberto Moya <lastresmoyallc@gmail.com>
2021-03-09 22:59:40 -0600

    cleanup

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,17 +1,15 @@
 import React, {useState, useEffect} from 'react';
-import Head from 'next/head'
-import apiClient from '../../services/apiClient';
-import TagList from '../../components/Shared/Tags/TagList';
-import Navbar from '../../components/Shared/Navbar'
-import Footer from '../../components/Shared/Footer'
-import PortfolioItem from '../../components/Shared/Portfolio/PortfolioItem';
-import AOS from 'aos'
+import apiClient from 'services/apiClient';
+import TagList from 'components/_common/Tags/TagList';
+import PortfolioItem from 'components/_common/Portfolio/PortfolioItem';
+// import AOS from 'aos'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import SiteWrapper from 'components/_common/site-wrapper';
 
 export default function Home() {
-  useEffect(() => {	AOS.init()})
-	useEffect(() => {AOS.refresh()}, [])
+  // useEffect(() => {	AOS.init()})
+	// useEffect(() => {AOS.refresh()}, [])
 
   const [portfolio, setPortfolio ] = useState([])
 
@@ -46,30 +44,11 @@ export default function Home() {
 		}
   }
   return (
-    <div>
-      <Head>
-        <title>PengYi Labs Costa Rica</title>
-        <link rel="icon" href="/favicon.ico" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164970324-1"></script>
-        <script
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-164970324-1');`
-            }}
-          />
-      </Head>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WGCCSM"
-      height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
-      <div>
-        <div>
-          <Navbar />
-        </div>
-        
+    <SiteWrapper>
+
         <div className="container my-5">
         <Link href="/#portfolio" className="btn btn-link">BACK TO PORTFOLIO</Link>
-          {(portfolio && portfolio.length > 0) ? portfolio.map(item => 
+          {(portfolio && portfolio.length > 0) ? portfolio.map(item =>
           <div className="text-center">
               <h1 className="text-primary mb-3">{item.fields.PortfolioItem}</h1>
               <div className="mb-3">
@@ -96,9 +75,6 @@ export default function Home() {
               return <PortfolioItem {...rel.fields} key={rel.id}/>
             })}
         </div>
-     
-    <Footer />
-    </div>
-    </div>
+    </SiteWrapper>
   )
 }
