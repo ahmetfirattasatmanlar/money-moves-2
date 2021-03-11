a30e9099c93ac6469f0cee572a68550b0af6e516
Roberto Moya <lastresmoyallc@gmail.com>
2021-03-11 01:59:18 -0600

    OMG what a bunch of fixes

--- components/_common/Tags/all-tags/index.js
+++ components/_common/Tags/all-tags/index.js
@@ -12,8 +12,8 @@ export default function AllTags(props){
   const fetchData = async () =>{
     try {
       // Need to change THIS
-      const {data: taglist} = await apiClient.getAllPortfolio();
-      console.log(taglist.records)
+      const {data: taglist} = await apiClient.getTags();
+      // console.log(taglist.records)
       setTags(taglist.records)
       // })
 		} catch (e) {
@@ -24,66 +24,3 @@ export default function AllTags(props){
   return tags ? <TagList tags={tags}/> : null
 
 }
-
-
-
-// class AllTags extends Component {
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       tags: [],
-//       isMounted: false
-//     };
-//   }
-//   async fetchData(){
-//     try {
-//       const {data: portfolioList} = await apiClient.getAllPortfolio();
-//       this.setState({
-//         tags: this.getTags(portfolioList.records),
-//         isMounted: true
-//       })
-// 		} catch (e) {
-// 			if(e.response){
-// 				console.log('portfolio didnt load')
-// 			}
-// 		}
-//   }
-//   componentDidMount(){
-//     this.fetchData()
-//   }
-//   getTags(portfolioList){
-// 		let AllTags = {}
-// 		if(portfolioList){
-// 			portfolioList.map(project => {
-// 				project.fields['Tags'].map(tag => {
-// 					if(!(tag in AllTags)){
-// 						AllTags[tag] = 1
-// 					}
-// 					AllTags[tag] += 1
-// 					return false
-// 				},)
-// 				return false
-// 			})
-//     }
-// 		let AllTagsSorted = Object.keys(AllTags).sort(function(a,b){return AllTags[b]-AllTags[a]})
-// 		return AllTagsSorted
-//   }
-//   renderContent(){
-//     return(
-//       <div>
-//         {this.state.isMounted ?
-//           <TagList tags={this.state.tags}/>
-//           :
-//           <Spinner/>
-//         }
-//       </div>
-//     )
-//   }
-// 	render() {
-//     return(
-//       this.renderContent()
-//     )
-//   }
-// }
-
-// export default AllTags;
--- components/_common/portfolio-card/index.js
+++ components/_common/portfolio-card/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Link from 'next/link'
-import { Card } from 'antd';
-import TagList from 'components/_common/Tags/TagList';
+import { Card, Tag } from 'antd';
 
 const { Meta } = Card;
 
-export default function PortfolioCard({Tags, PortfolioId, Screens, PortfolioItem}){
-  // {Tags, PortfolioId, Screens, PortfolioItem}
+export default function PortfolioCard({Screens, PortfolioItem, DynamicTags}){
+  // {DynamicTags ? console.log(DynamicTags) : null}
+
   let renderThumb = () => {
     if(Screens.length > 0 ){
       return Screens[0].thumbnails.large.url || DataTransferItemList.Screens[0].thumbnails.large.url;
@@ -30,7 +30,9 @@ export default function PortfolioCard({Tags, PortfolioId, Screens, PortfolioItem
               <div className="d-flex align-items-center">
                 <div className="w-100">
                   <div className="ant-card-meta-title">{PortfolioItem}</div>
-                  <TagList tags={Tags.splice(0,2)} disabled/>
+                  {DynamicTags && DynamicTags.length > 0 ? DynamicTags.splice(0,2).map(tag=> {
+                    return <Tag key={tag}><Link href={`/portfolio/tags/${tag}`} className="text-primary badge badge-sm">{tag}</Link></Tag>
+                  }) : undefined}
                 </div>
               </div>
             }
