8a029f34b0c6d3431d7b420d9a4a1930502684f9
Roberto Moya <lastresmoyallc@gmail.com>
2021-04-01 14:37:17 -0600

    added google tag manager

--- components/_common/site-wrapper/index.js
+++ components/_common/site-wrapper/index.js
@@ -23,15 +23,22 @@ export default function SiteWrapper({children}){
       <Head>
         <title>PengYi Labs Costa Rica | Singapore</title>
         <link rel="icon" href="/favicon.ico" />
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5WGCCSM');`}}/>
       </Head>
+      {/* Google Tag Manager (noscript) */}
+      <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WGCCSM"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}/>
+      {/* End Google Tag Manager (noscript) */}
       {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WGCCSM"
       height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript> */}
       <Navbar />
       {children}
       <Footer/>
       {showChat && <MessengerCustomerChat pageId="101921091461361" appId="404665513945389"/>}
-      {/* {window.location.hostname === 'pengyilabs.io' ?  : undefined} */}
-
     </>
   )
 }
