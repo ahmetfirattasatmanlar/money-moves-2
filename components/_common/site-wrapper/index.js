4c779818b6234369b3b300b8f2c2ae4a218f5ed7
rob Moya <lastresmoyallc@gmail.com>
2021-04-20 10:53:25 -0500

    google tag

--- components/_common/site-wrapper/index.js
+++ components/_common/site-wrapper/index.js
@@ -23,6 +23,14 @@ export default function SiteWrapper({children}){
       <Head>
         <title>PengYi Labs Costa Rica | Singapore</title>
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164970324-1"></script>
+        <script dangerouslySetInnerHTML ={{__html:
+          `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('set', {'user_id': 'USER_ID'});
+          gtag('config', 'UA-164970324-1');`
+        }}/>
         <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
