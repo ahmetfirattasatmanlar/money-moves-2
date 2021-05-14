80fc5d9bfb7018973cce4e2855e503d27641218b
Roberto Moya <lastresmoyallc@gmail.com>
2021-05-14 09:39:45 -0500

    login

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,11 @@ import '../styles/App.scss'
 import React from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className="bg-white">
+    <div className="mx-auto bg-gradient" style={{maxWidth: '25rem', minHeight: '100vh'}}>
+      <Component {...pageProps} />
+    </div>
+  </div>
 }
 
 export default MyApp
--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,55 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Login() {
+
+  const [withEmail, isWithEmail] = useState(false)
+
   return (
-    <div>
-      <img src="/logo-moneymoves.svg" />
+    <div className="py-5 text-center px-4">
+      <img src="/logo-moneymoves.svg"  className="mt-4"/>
 
-      Don't have an account?
-      <Link href="/register">
-        <a>Register</a>
-      </Link>
+      <div className={withEmail ? 'pt-4 pb-2' : 'py-5'}>
+        <h1 className="h3">Sign In</h1>
+        <p>
+          Don't have an account?
+          <Link href="/register">
+            <a> Register</a>
+          </Link>
+        </p>
+      </div>
+
+      <button className="btn btn-light bg-white shadow btn-block  py-3 mb-3" style={{borderRadius: '10rem'}}>Sign In with Google</button>
+      <button className="btn btn-light bg-white shadow btn-block py-3 " style={{borderRadius: '10rem'}}>Sign In with Facebook</button>
 
 
-      <Link href="/recover-password">
-        <a>Forgot Password</a>
-      </Link>
+      {!withEmail ?
+        <p className="btn-link text-decoration-underline mt-5" onClick={()=>isWithEmail(!withEmail)}>Our use your email account</p>
+        :
+        <p className="pt-4 text-muted">---- Our use your email account ----</p>
+      }
 
-      <Link href="/home">
-        <a>Sign In</a>
-      </Link>
+
+      {withEmail ? <form>
+
+        <div className="mb-3">
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username or email"/>
+          {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+        </div>
+        <div className="mb-3">
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
+
+        <div className="d-flex justify-content-end mb-4">
+          <Link href="/recover-password">
+            <a>Forgot Password</a>
+          </Link>
+        </div>
+
+        <Link href="/home">
+          <a className="btn btn-primary py-3 btn-block" style={{borderRadius: '10rem'}}>Sign In</a>
+        </Link>
+      </form> : undefined}
 
 
     </div>
