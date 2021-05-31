4f0793817ade81c0ab6788586b0fd5bfacfda60e
Roberto Moya <lastresmoyallc@gmail.com>
2021-05-30 20:42:41 -0500

    bank

--- components/_common/app-wrapper.js
+++ components/_common/app-wrapper.js
@@ -2,7 +2,7 @@ import Nav from "./nav";
 
 export default function AppWrapper({ nav = false, children, dark = false }) {
   return (
-    <div className={`d-flex flex-column justify-content-between ${dark ? 'bg-gradient-dark h-100 pb-0' : ''}`} style={{ paddingBottom: '5rem' }}>
+    <div className={`d-flex flex-column justify-content-between ${dark ? 'bg-gradient-dark pb-0' : ''}`} style={{ marginBottom: '5rem' }}>
       {children}
       {nav ? <Nav /> : undefined}
     </div>
--- pages/verify/banking.js
+++ pages/verify/banking.js
@@ -1,12 +1,49 @@
-import Link from 'next/link'
+// import Link from 'next/link'
+import AppWrapper from 'components/_common/app-wrapper'
+import BackButton from 'components/_common/back-button'
+import { FaCheck, FaChevronRight } from "react-icons/fa";
 
 export default function Banking() {
   return (
-    <div>
-      <Link href="/verify">
-        <a>Back</a>
-      </Link>
-      Verify Banking Info
-    </div>
+    <AppWrapper>
+      <div className="d-flex flex-column h1-00" >
+        <div className="px-3 mb-5 d-flex justify-content-between align-items-center">
+          <BackButton />
+          <strong className="pr-4">Bank Verification</strong>
+          <span>&nbsp;</span>
+        </div>
+        <div className="px-3">
+          <p>FLINKS Login screens</p>
+          <div className="card rounded-lg border-0 shadow mb-4">
+            <div className="card-body d-flex align-items-center">
+              <img src="/icon-hsbc.svg" alt="" className="mr-3" />
+              HSBC
+              <span className="ml-auto"><FaChevronRight /></span>
+            </div>
+          </div>
+          <div className="card rounded-lg border-0 shadow mb-4">
+            <div className="card-body d-flex align-items-center">
+              <img src="/icon-tdbank.svg" alt="" className="mr-3" />
+              TD Bank
+              <span className="ml-auto ">
+                <span className="text-success mr-3 "><FaCheck />&nbsp;Verified</span>
+                <FaChevronRight />
+              </span>
+            </div>
+          </div>
+          <div className="card rounded-lg border-0 shadow mb-4">
+            <div className="card-body d-flex align-items-center">
+              <img src="/icon-scottia.svg" alt="" className="mr-3" />
+              Scottia Bank
+              <span className="ml-auto"><FaChevronRight /></span>
+            </div>
+          </div>
+        </div>
+        <div className="mt-auto px-2 pb-4">
+          <button className="btn btn-primary btn-block rounded-lg">Confirm</button>
+        </div>
+      </div>
+
+    </AppWrapper>
   )
 }
