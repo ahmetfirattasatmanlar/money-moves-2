518fc732a8a3a94aa1e5dac1951d005131e4b16a
Roberto Moya <lastresmoyallc@gmail.com>
2021-05-30 20:23:51 -0500

    features

--- components/_common/features.js
+++ components/_common/features.js
@@ -1,24 +1,20 @@
-export default function FeaturesContainer(props) {
-  const { cards, title } = props;
-
-  const featureCard = (text, index) => {
-    const savings = text === "Saving Goals";
-    const images = ["/savings.svg", "/budgets.svg", "/subscriptions.svg"];
-    return (
-      <div className="d-flex align-items-center p-3">
-        <img src={images[index]} alt="" className="mr-3" />
-        <strong>{text}</strong>
-        <img src={`${savings ? "/start.svg" : "/caret.svg"}`} alt="" className="ml-auto" />
-      </div>
-    );
-  };
+export default function FeaturesContainer({ cards, title }) {
 
   return (
     <div className="card mb-2 border-0 shadow-sm rounded-lg">
-      {title ? <div className="card-header bg-transparent pb-0 border-0"><h2 className="h6 font-weight-bold">{title}</h2></div> : undefined}
+      {title ? <div className="card-header bg-transparent pb-0 border-0"><h2 className="h5 font-weight-bold">{title}</h2></div> : undefined}
+
       {cards.map((card, index) => {
-        return featureCard(card, index);
+        // console.log(card)
+        const images = ["/savings.svg", "/budgets.svg", "/subscriptions.svg"];
+        const savings = card === "Saving Goals";
+
+        return <div className="d-flex align-items-center p-3" key={`feature-${index}`}>
+          <img src={images[index]} alt="" className="mr-3" />
+          <strong>{card}</strong>
+          <img src={`${savings ? "/start.svg" : "/caret.svg"}`} alt="" className="ml-auto" />
+        </div>
       })}
-    </div>
+    </div >
   );
 }
